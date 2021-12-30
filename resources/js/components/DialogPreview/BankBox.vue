eec48ee603b3a595a07431d7ad82cf3eb869255a
brylle <kapnudel007@gmail.com>
2021-12-30 10:55:18 +0800

    initial

--- resources/js/components/DialogPreview/BankBox.vue
+++ resources/js/components/DialogPreview/BankBox.vue
@@ -27,14 +27,14 @@
               
                     <span>{{operatorName}}</span>
                     <!-- <span>{{banks}}</span> -->
-                    <span v-if="!editmode" > {{banks[0].bank_name}}</span>
+                    <span v-if="!editmode" > {{ bankSelected.bank_name || 'No Bank' }}</span>
                     <span  class="select-field_container" :class="{'editmode-span': editmode}">
                         <select v-if="editmode"  class="small sign-name select-field_report"  @change="filterbank($event)" >
                             <!-- <option default disabled :value="banks[0].id" >{{banks[0].bank_name}}</option> -->
-                            <option v-for="bank in banks" :key="bank.id"  :value="bank.id" >{{bank.bank_name}}</option>
+                            <option v-for="bank in arenaDetails.bank_details" :key="bank.id"  :value="bank.id" >{{bank.bank_name}}</option>
                         </select>
                     </span>
-                    <span>{{banks[0].bank_number}}</span>
+                    <span>{{ bankSelected.bank_number || 'No Bank' }}</span>
                    
                 </div> 
                 
@@ -48,31 +48,46 @@
         name: "BankBox",
         props:{
             bank:Object,
-            banks:Array,
-            b: Object,
+           
+        
+            arenaDetails: Object,
             arenaId: Object,
             bankId: Object,
+           
             operatorName: String,
             editmode: Boolean,
             depositReplenishText: Object
         },
+        data(){
+            return {bankSelected: {}}
+        },
         methods: {
            
             filterbank(event){
-                console.log(event.target.value)
-                //   axios.get("api/filterBank/"+ this.bank.bank_id).then(({data}) => (
-                //        this.bank.bank_name = data.bank_name,
-                //         this.bank.bank_number = data.bank_number
-                //     ));
 
                 axios.put('api/updateBankSelection/'+this.arenaId.id, {
                     bank_id: event.target.value
                 })
             },
             arenaSelectedBank(){
-                  axios.get(`api/arenaSelectedBank/${parseInt(this.bankId.id)}`).then(data => {
-                      console.log('ARENA SELECTED',data)
-                  })
+                
+                    if(this.bankId.id) {
+                        const bb = this.arenaDetails.bank_details.length > 0 ? parseInt(this.arenaDetails.bank_details[0].id) : null
+                        const bId = this.bankId.id ? this.bankId.id : bb
+                        
+                        axios.get(`api/arenaSelectedBank/${bId}`).then(({data}) => {
+                            this.bankSelected = data
+                            console.log('BANK ARENA SELECTED',data)
+                        })
+                    }else {
+                        this.bankSelected = {
+                            name: this.arenaDetails.bank_details.length > 0 ? this.arenaDetails.bank_details[0].bank_name : null,
+                            number: this.arenaDetails.bank_details.length > 0 ? this.arenaDetails.bank_details[0].bank_number : null
+                        }
+                    }
+                   
+                
+                    
             }
         },
         created(){
