791a78037c0f8e0701e29ddfb87392f3f1b49c8c
brylle <kapnudel007@gmail.com>
2022-02-22 14:20:20 +0800

    fixin

--- resources/js/components/SOA.vue
+++ resources/js/components/SOA.vue
@@ -28,8 +28,53 @@
                         
                         <v-spacer></v-spacer>
                         <!-- FILE INPUT -->
-                        <soa-input></soa-input>
-                    
+                        <!-- <soa-input></soa-input> -->
+                        <v-col class="col-md-4" v-show="$gate.isAdmin()">
+                            <v-file-input
+                                outlined
+                                dense
+                                v-model="fileUpload"
+                                color="deep-purple accent-4"
+                                label="File input"
+                                placeholder="Select your file"
+                                :clearable="false"
+                                counter
+                                
+                                append-icon="mdi-file-import"
+                                :show-size="1000"
+                                @change="onFileChange($event)"
+                            >
+                                <template v-slot:append>
+                                    <v-tooltip bottom color="success">
+                                        <template v-slot:activator="{ on }">
+                                            <v-icon
+                                                large
+                                                :disabled="!isExcel"
+                                                v-on="on"
+                                                color="green darken-3"
+                                                style="cursor: pointer"
+                                                @click="proceedAction"
+                                            >
+                                                mdi-file-import
+                                            </v-icon>
+                                        </template>
+                                        <span>Import File</span>
+                                    </v-tooltip>
+                                </template>
+                                <template v-slot:selection="{ index, text }">
+                                    <v-chip
+                                        v-if="index < 2"
+                                        color="deep-purple accent-4"
+                                        dark
+                                        label
+                                        close
+                                        @click:close="clearFile"
+                                    >
+                                        {{ text }}
+                                    </v-chip>
+                                </template>
+                            </v-file-input>
+                        </v-col>
                     </v-row>
                     <v-card>
                         <v-card-title>
@@ -979,6 +1024,11 @@ export default {
             );
             this.ocbsArrayFiltered = arenaReportFiltered;
             this.isExcel = isExcel;
+        },
+         clearFile(file) {
+            console.log(file);
+            this.isExcel = false;
+            this.fileUpload = null;
         },
 
         proceedAction() {
